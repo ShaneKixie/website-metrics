58b2b6a6e6ba70e0240b96ffb0a30ca263fb9c85
ShaneKixie <168578928+ShaneKixie@users.noreply.github.com>
2026-03-14 13:20:49 -0700

    feat: register pattern discovery tools in tool registry

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,9 +1,11 @@
 import { registerAccountTools } from "./accountTools.js";
 import { registerGscTools } from "./gscTools.js";
 import { registerGa4Tools } from "./ga4Tools.js";
+import { registerAnalysisTools } from "./analysisTools.js";
 
 export function registerAllTools(server) {
   registerAccountTools(server);
   registerGscTools(server);
   registerGa4Tools(server);
+  registerAnalysisTools(server);
 }
